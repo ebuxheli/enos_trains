c74193c8e88a029f6860e3066a4545a1a19d405a
Enxhi Buxheli <enxhibuxheli@Enxhis-MacBook-Pro.local>
2019-02-20 00:16:32 -0500

    took out xtable output to tex

--- dataverse_files/balance_check.r
+++ dataverse_files/balance_check.r
@@ -2,19 +2,10 @@
 ###check to see if randomization created balanced samples in Enos experiment
 ####RdE September 2012
 
-# Commenting out the junk from the xtable file creation
-options(xtable.comment = FALSE)
-
 out.balance.test = xBalance(fmla = treatment ~ liberal+republican+obama.disapprove+ride.everyday+voted.2010+romney.voter+Hispanics.x+age+residency.new+hispanic.new+college+income.new+male+white, 
                             data = dat.all, 
                             report = c("std.diffs","z.scores","adj.means","chisquare.test"), 
                             strata = factor(dat.all$station))
-
-## Outputs the results for Table 2 in the replication document
-xtable.out.balance.text = xtable(out.balance.test)
-balance_table <- print(xtable.out.balance.text, 
-                       file = 'balance_table.tex',
-                       floating = FALSE)
 
 missing.balance.test = xBalance(fmla = missing ~ liberal+republican+obama.disapprove+ride.everyday+voted.2010+romney.voter+Hispanics+age+residency.new+hispanic.new+gender+college+us.born+income.new+male, 
                                 data = dat.t1, 
